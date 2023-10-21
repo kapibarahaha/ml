7bfaa8bbb5c4498a8e0f6b2b849921955ee85f33
kapibarahaha <148653631+kapibarahaha@users.noreply.github.com>
2023-10-22 03:46:34 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
-st.title("ควยมิว ควยหมวย")
+st.title("Machine Learning Project")
 import streamlit as st
 
-st.title('Web Application สำหรับกรอกข้อมูล')
+st.title('กรอกข้อมูล ตามหัวข้อ')
 
 # สร้างส่วนของแบบฟอร์ม
-user_input = st.text_input('กรอกข้อมูล:')
-
+user_input = st.text_input('กรอกค่า Do:')
+user_input = st.text_input('กรอกค่า Color:')
+user_input = st.text_input('กรอกค่า Smell:')
+user_input = st.text_input('กรอกค่า TDS(ppm):')
+user_input = st.text_input('กรอกค่า EC(microsecond/cm):')
+user_input = st.text_input('กรอกค่า PH:')
+user_input = st.text_input('กรอกค่า Area:')
+user_input = st.text_input('กรอกค่า Time:')
 if st.button('ส่งข้อมูล'):
     st.write(f'คุณกรอก: {user_input}')
